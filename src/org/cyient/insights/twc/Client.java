5942c11c626b2e7254fe326121e3af351885a783
Sreekanth Asodi <asodi.sreekanth@gmail.com>
2016-03-30 08:37:21 +0530

    Ready for first test

--- src/org/cyient/insights/twc/Client.java
+++ src/org/cyient/insights/twc/Client.java
@@ -1,8 +1,16 @@
 package org.cyient.insights.twc;
 
+import com.serotonin.bacnet4j.LocalDevice;
+import com.serotonin.bacnet4j.npdu.ip.IpNetwork;
+import com.serotonin.bacnet4j.transport.DefaultTransport;
+import com.serotonin.bacnet4j.transport.Transport;
 import com.thingworx.communications.client.ClientConfigurator;
 import com.thingworx.communications.client.ConnectedThingClient;
+import com.thingworx.communications.client.things.VirtualThing;
 import com.thingworx.communications.common.SecurityClaims;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by sriky on 29/3/16.
@@ -33,18 +41,50 @@ public class Client extends ConnectedThingClient {
         config.setAsSDKType();
 
         Client client = new Client(config);
-        /* TODO create a list of blocks initialize and bind them. need to use the node and device classes.*/
+
+        IpNetwork network = new IpNetwork("192.168.168.255", 47808, "192.168.168.120");
+        Transport transport = new DefaultTransport(network);
+        LocalDevice localDevice = new LocalDevice(23422, transport);
+        List<Node> listOfNodes = new ArrayList<Node>();
+
+        /* create a list of blocks initialize and bind them. need to use the node and device classes.*/
+        /*for each device in the list initiate the node objects*/
+        for(int deviceID: Devices.getAsList()) {
+            listOfNodes.add(new Node(localDevice, deviceID, Devices.getActiveNames()));
+            System.out.println("processing device: " + deviceID);
+        }
+
+        for(Node node: listOfNodes){
+            client.bindThing(new Thing(node.getBlockName(),node.getBlockName(),node.getBlockName(),client,node,localDevice));
+        }
 
         try {
             // Start the client
             client.start();
+            localDevice.initialize();
         }
         catch(Exception eStart) {
             System.out.println("Initial Start Failed : " + eStart.getMessage());
         }
 
-        /* TODO loop over all the blocks and collect and set data.*/
-
+        /* loop over all the blocks and collect and set data.*/
+        // As long as the client has not been shutdown, continue
+        while(!client.isShutdown()) {
+            // Only process the Virtual Things if the client is connected
+            if(client.isConnected()) {
+                // Loop over all the Virtual Things and process them
+                for(VirtualThing thing : client.getThings().values()) {
+                    try {
+                        thing.processScanRequest();
+                    }
+                    catch(Exception eProcessing) {
+                        System.out.println("Error Processing Scan Request for [" + thing.getName() + "] : " + eProcessing.getMessage());
+                    }
+                }
+            }
+            // Suspend processing at the scan rate interval
+            Thread.sleep(1000);
+        }
 
     }
 }
---
+++ src/org/cyient/insights/twc/Thing.java
@@ -0,0 +1,87 @@
+package org.cyient.insights.twc;
+
+/**
+ * Created by sriky on 30/3/16.
+ */
+
+import com.serotonin.bacnet4j.LocalDevice;
+import com.thingworx.communications.client.ConnectedThingClient;
+import com.thingworx.communications.client.things.VirtualThing;
+import com.thingworx.metadata.FieldDefinition;
+import com.thingworx.metadata.annotations.ThingworxPropertyDefinition;
+import com.thingworx.metadata.annotations.ThingworxPropertyDefinitions;
+import com.thingworx.metadata.annotations.ThingworxServiceDefinition;
+import com.thingworx.metadata.annotations.ThingworxServiceResult;
+import com.thingworx.metadata.collections.FieldDefinitionCollection;
+import com.thingworx.types.BaseTypes;
+import com.thingworx.types.constants.CommonPropertyNames;
+
+import java.util.Map;
+
+// Property Definitions
+@SuppressWarnings("serial")
+@ThingworxPropertyDefinitions(properties = {
+        @ThingworxPropertyDefinition(name="ratemp", description="Return air Temperature", baseType="NUMBER", category="block", aspects={"isReadOnly:true"}),
+        @ThingworxPropertyDefinition(name="satemp", description="Supply air Pressure", baseType="NUMBER", category="block", aspects={"isReadOnly:true"}),
+        @ThingworxPropertyDefinition(name="raco2", description="return air co2", baseType="NUMBER", category="block", aspects={"isReadOnly:true"}),
+
+})
+public class Thing extends VirtualThing implements Runnable  {
+    private Map<String,Float> values = null;
+    private Thread _shutdownThread = null;
+    private Node node = null;
+    private LocalDevice localDevice =null;
+    public Thing(String name, String description, String identifier, ConnectedThingClient client, Node node, LocalDevice localDevice) {
+        super(name,description,identifier,client);
+        super.initializeFromAnnotations();
+        this.init();
+        this.node = node;
+    }
+    private void init()
+    {
+        initializeFromAnnotations();
+    }
+    @Override
+    public void processScanRequest() throws Exception {
+        // Be sure to call the base classes scan request
+        super.processScanRequest();
+        // Execute the code for this simulation every scan
+        this.scanDevice();
+    }
+    public void scanDevice() throws Exception {
+        values = node.getPresentValues(localDevice);
+        super.setProperty("satemp", values.get("sa_temp_1"));
+        super.setProperty("ratemp", values.get("ra_temp_1"));
+        super.setProperty("raco2", values.get("ra_co2_1"));
+        System.out.println("Done settign property");
+
+        // Update the subscribed properties and events to send any updates to Thingworx
+        // Without calling these methods, the property and event updates will not be sent
+        // The numbers are timeouts in milliseconds.
+        super.updateSubscribedProperties(1000);
+        super.updateSubscribedEvents(1000);
+    }
+
+    @ThingworxServiceDefinition( name="Shutdown", description="Shutdown the client")
+    @ThingworxServiceResult( name=CommonPropertyNames.PROP_RESULT, description="", baseType="NOTHING")
+    public synchronized void Shutdown() throws Exception {
+        // Should not have to do this, but guard against this method being called more than once.
+        if(this._shutdownThread == null) {
+            // Create a thread for shutting down and start the thread
+            this._shutdownThread = new Thread(this);
+            this._shutdownThread.start();
+        }
+    }
+    @Override
+    public void run() {
+        try {
+            // Delay for a period to verify that the Shutdown service will return
+            Thread.sleep(1000);
+            // Shutdown the client
+            this.getClient().shutdown();
+        } catch (Exception x) {
+            // Not much can be done if there is an exception here
+            // In the case of production code should at least log the error
+        }
+    }
+}
